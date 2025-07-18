a3d6d9f3d2f7144a2491baa62e8e8df37abcf154
kageurarenn <241020@std.hi-joho.ac.jp>
2025-07-18 10:56:40 +0900

    最終課題

--- src/Goblin.java
+++ src/Goblin.java
@@ -1,11 +1,12 @@
-public class Goblin extends Monster{
-    Goblin(char suffix, int hp){
-        super("ゴブリン",suffix, hp);
+public class Goblin extends Monster {
+    Goblin(char suffix, int hp) {
+        super("ゴブリン", suffix, hp);
     }
 
-    public void attack(Creature target){
-        System.out.println( getName() +getSuffix()+"はナイフで攻撃!" +target.getName() + "に8のダメージを与えた!");
-        target.setHp(target.getHp()- 8) ;
+    public void attack(Creature target) {
+        System.out.println(getName()+ getSuffix() + "は体当たり攻撃！" + target.getName() + "に8のダメージを与えた！");
+        target.setHp(target.getHp() - 8);
     }
+
 
 }
